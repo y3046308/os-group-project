80cef03fbf461081e1eb89340fdc8fa7292a5828
y3046308 <y3046308@gmail.com>
2013-10-23 02:26:06 +0000

    added int sys_close(int fd)

--- os161-1.99/kern/include/syscall.h
+++ os161-1.99/kern/include/syscall.h
@@ -63,6 +63,7 @@ int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 #if OPT_A2
 int sys_read(int fd, void *buf, size_t buflen);
 int sys_write(int fd, const void *buf, size_t nbytes);
+int sys_close(int fd);
 void sys__exit(int exitcode);
 #endif
 
