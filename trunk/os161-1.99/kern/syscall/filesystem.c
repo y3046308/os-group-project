955ac16281fa3a799e9acced1b08eff7eb5b8335
qorcjftns2@gmail.com <qorcjftns2@gmail.com@280eca72-8607-5fb9-e67e-0985319f605b>
2013-10-26 03:01:11 +0000

    seperates file system calls and process calls

--- trunk/os161-1.99/kern/syscall/filesystem.c
+++ trunk/os161-1.99/kern/syscall/filesystem.c
@@ -157,14 +157,4 @@ pid_t sys_getpid(){
     return curthread->t_proc->t_pid;
 }
 
-void sys__exit(int exitcode) {
-	threadarray_remove(&curproc->p_threads, 0);
-	proc_destroy(curthread->t_proc);
-	curthread->t_proc = NULL;
-	thread_exit();
-	// code below is removed. Since the process is destroyed, there is no "curthread" now.
-	// We cannot access to it.
-	// curthread->t_proc = NULL;
-	(void)exitcode;
-}
 #endif
