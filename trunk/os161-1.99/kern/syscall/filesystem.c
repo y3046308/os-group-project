b84622561c7690842c8607d54cc09927c9bc9985
soojilee13@gmail.com <soojilee13@gmail.com@280eca72-8607-5fb9-e67e-0985319f605b>
2013-11-03 05:41:07 +0000

    sys_open fixed and comments added for readabilty, and unsued functions moved to the bottom

--- trunk/os161-1.99/kern/syscall/filesystem.c
+++ trunk/os161-1.99/kern/syscall/filesystem.c
@@ -14,21 +14,12 @@
 #include "opt-A2.h"
 #include "synch.h"
 #include "array.h"
+
 #if OPT_A2
 
 #define MAX_fd_table 64	// only allow 64 files in fd_table (size of TLB)
 
 int errno;
-
-/*#ifndef FDINLINE
-#define FDINLINE INLINE
-#endif
-
-DECLARRAY(fd);
-DEFARRAY(fd, FDINLINE);
-
-volatile int i = 0; //index of fd
-struct fdarray* fd_table = NULL;  // collection of file descriptor*/
 
 static struct fd* create_fd(int flag, int handle, const char* filename, struct vnode* vn){
 	struct fd* file_descriptor;
@@ -53,62 +44,6 @@ static void add_fd(struct fd* file){		// add new file descriptor to fd_table
 	curproc->fd_table[i] = file;
 }
 
-// find_fd function is commented out because we have no use for it so far. Uncomment and use function if necessary
-
-/*static struct fd* find_fd(char* filename){    // find f.d with given fh
-  struct fd** copy = curproc->fd_table;
-  for(int i =0; i < MAX_fd_table; i++){
-    if (strcmp((copy[i])->filename, filename) == 0){
-      return copy[i];
-    }
-  }
-  return NULL;           // fd not found
-}*/
-
-
-
-/*struct fd* find_fd_name(const char* name){    // find f.d with given file name
-  struct fd* copy = fd_table;
-  while (copy != NULL){
-    if (copy->filename == name){
-      return copy;
-    }
-    copy = copy + 1;
-  }
-  return NULL;           // fd not found
-}*/
-
-/*int sys_open(const char* filename, int flags) {
-	int open = -1;
-	if (filename  == NULL) errno = EFAULT; // filename was an invalid pointer.
-
-	else if (flags != O_RDONLY || flags != O_WRONLY || flags != O_RDWR || flags != O_CREAT || 
-            flags != O_EXCL || flags != O_TRUNC || flags != O_APPEND) errno = EINVAL; //		flags contained invalid values.
-        else{    
-		struct fd* tmp = find_fd_name(filename);             // find a file from file description fd_table
-		if (tmp == NULL){
-			if(flags != O_CREAT) errno = ENOENT;  // The named file does not exist, and O_CREAT was not specified.
-			else{					// if flag is O_CREAT, create a file
-				int newFlag;
-				for (newFlag = 3; newFlag < MAX_fd_table ; newFlag++){
-					if ((fd_table + newFlag) == NULL){
-						break;		// if fd_table at [newFlag] does not exit, it will be used to store new f.d
-					}
-				} 
-				add_fd(create_fd(newFlag, (char*)filename, NULL));	// create a new fd and add it into fd_table
-				open = newFlag;
-			}
-		}
-		else{
-			if(flags == O_EXCL) errno = EEXIST; // The named file exists, and O_EXCL was specified.
-			else{
-		  		open = vfs_open((char*)filename, flags, 0, &tmp->file);        // actually open a file
-			}
-		}
-	}
-	return open;
-}*/
-
 int sys_close(int fd){
   struct vnode* tmp = curproc->fd_table[fd]->file;
   if (tmp != NULL){
@@ -131,39 +66,36 @@ int sys_open(const char *filename, int file_flag, mode_t mode){
 		return -1;
 	}
 	
-	struct vnode** new_file = NULL;
-	int ret;
-	char* filenamecast = strcpy(filenamecast, filename);
-	ret = vfs_lookup(filenamecast, new_file);
-	if(file_flag & O_CREAT && ret != -1 && file_flag & O_EXCL){
-		errno = EEXIST;
+	struct vnode** new_file = NULL; //passed into vfs_lookup function
+	int ret; //return value
+	char* filenamecast = strcpy(filenamecast, filename); //cast constant filename -> non-constant
+	
+	ret = vfs_lookup(filenamecast, new_file); //takes care of EISDIR and ENOTDIR hopefully
+	if(file_flag & O_CREAT && ret >= 0 && file_flag & O_EXCL){
+		errno = EEXIST; //file creation failed because it already exists
 		return -1;
 	}
 	
-	//vfs_lookup(filename, new_file); //takes care of EISDIR and ENOTDIR
-	
-	new_file = NULL;
-	if(curproc->open_num < MAX_fd_table){  //Too many opened files
+	new_file = NULL; //passed into vfs_open function
+	if(curproc->open_num < MAX_fd_table){  //fd table is not full, open allowed 
 		ret = vfs_open(filenamecast, file_flag, mode , new_file);
 		curproc->open_num++;
 		if(ret < 0){
 			return -1;
 		}		
-	}else{
-		errno = EMFILE;
+	}else{ //fd table is full, open disallowed
+		errno = EMFILE; 
 		return -1;
 	}
 	
-	//still need to check errors: ENOSPC and EIO	
+	//still need to check errors: ENOSPC and EIO********************************************************************	
 
-	int i = 0;
-	while(curproc->fd_table != NULL) {
-		i++;
+	int file_handle = 0; //file handle is the index at which the fd is located
+	while(curproc->fd_table[file_handle] != NULL) { //find empty slot in fd_table
+		file_handle++;
 	}
 
-	int file_handle = curproc->fd_table[i]->file_handle;
-
-	struct fd* f = create_fd(file_flag, file_handle, filename, *new_file);
+	struct fd* f = create_fd(file_flag, file_handle, filename, *new_file); //add fd to the fd table
 	add_fd(f);
 
 	return 0;  //index of the fd in the fd_fd_table
@@ -209,5 +141,60 @@ int sys_write(int fd, const void *buf, size_t nbytes) {
 
 	return result;
 }
+
+
+/*struct fd* find_fd_name(const char* name){    // find f.d with given file name
+  struct fd* copy = fd_table;
+  while (copy != NULL){
+    if (copy->filename == name){
+      return copy;
+    }
+    copy = copy + 1;
+  }
+  return NULL;           // fd not found
+}*/
+
+/*int sys_open(const char* filename, int flags) {
+    int open = -1;
+    if (filename  == NULL) errno = EFAULT; // filename was an invalid pointer.
+
+    else if (flags != O_RDONLY || flags != O_WRONLY || flags != O_RDWR || flags != O_CREAT || 
+            flags != O_EXCL || flags != O_TRUNC || flags != O_APPEND) errno = EINVAL; //        flags contained invalid values.
+        else{    
+        struct fd* tmp = find_fd_name(filename);             // find a file from file description fd_table
+        if (tmp == NULL){
+            if(flags != O_CREAT) errno = ENOENT;  // The named file does not exist, and O_CREAT was not specified.
+            else{                   // if flag is O_CREAT, create a file
+                int newFlag;
+                for (newFlag = 3; newFlag < MAX_fd_table ; newFlag++){
+                    if ((fd_table + newFlag) == NULL){
+                        break;      // if fd_table at [newFlag] does not exit, it will be used to store new f.d
+                    }
+                } 
+                add_fd(create_fd(newFlag, (char*)filename, NULL));  // create a new fd and add it into fd_table
+                open = newFlag;
+            }
+        }
+        else{
+            if(flags == O_EXCL) errno = EEXIST; // The named file exists, and O_EXCL was specified.
+            else{
+                open = vfs_open((char*)filename, flags, 0, &tmp->file);        // actually open a file
+            }
+        }
+    }
+    return open;
+}*/
+
+// find_fd function is commented out because we have no use for it so far. Uncomment and use function if necessary
+
+/*static struct fd* find_fd(char* filename){    // find f.d with given fh
+  struct fd** copy = curproc->fd_table;
+  for(int i =0; i < MAX_fd_table; i++){
+    if (strcmp((copy[i])->filename, filename) == 0){
+      return copy[i];
+    }
+  }
+  return NULL;           // fd not found
+}*/
 
 #endif
