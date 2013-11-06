1ad6e124c73d84705a11adf4f9306d486452533d
y3046308 <y3046308@gmail.com>
2013-11-06 05:43:08 +0000

    added errnos

--- os161-1.99/kern/syscall/filesystem.c
+++ os161-1.99/kern/syscall/filesystem.c
@@ -39,6 +39,7 @@ static void add_fd(struct fd* file, int file_handle){		// add new file descripto
 }
 
 int sys_close(int fd){
+	// still needs to manage EIO error case
         int retval = -1;
         if (fd < 0 || fd >= MAX_fd_table){
                 errno = EBADF;
@@ -53,50 +54,57 @@ int sys_close(int fd){
 }
 
 int sys_open(const char *filename, int file_flag, mode_t mode){
-	if(filename == NULL){ //bad memory reference
-		errno = EFAULT;
-		return -1;
-	}
-	if(file_flag & O_APPEND){ //flags contained invalid values
-		errno = EINVAL;
-		return -1;
-	}
-	
-	struct vnode** new_file; //passed into vfs_lookup function
-	int ret; //return value
-	char* filenamecast = kmalloc(sizeof((char *) NAME_MAX));
-	strcpy(filenamecast, filename); //cast constant filename -> non-constant
-	
-	ret = vfs_lookup(filenamecast, new_file); //takes care of EISDIR and ENOTDIR hopefully
-	if(file_flag & O_CREAT && ret >= 0 && file_flag & O_EXCL){
-		errno = EEXIST; //file creation failed because it already exists
-		return -1;
-	}
-	
-	//new_file = NULL; //passed into vfs_open function
-	if(curproc->open_num < MAX_fd_table){  //fd table is not full, open allowed 
-		ret = vfs_open(filenamecast, file_flag, mode , new_file);
-		curproc->open_num++;
-		if(ret < 0){
-			return -1;
-		}
-	}else{ //fd table is full, open disallowed
-		errno = EMFILE; 
-		return -1;
-	}
-	
-	//still need to check errors: ENOSPC and EIO********************************************************************	
-	
-	int file_handle = 3; //file handle is the index at which the fd is located
-	
-	while(curproc->fd_table[file_handle] != NULL) { //find empty slot in fd_table
-		file_handle++;
-	}
+	bool create = false;
+        if(filename == NULL){ //bad memory reference
+                errno = EFAULT;
+                return -1;
+        }
+        if(file_flag & O_APPEND){ //flags contained invalid values
+                errno = EINVAL;
+                return -1;
+        }
+        
+        struct vnode** new_file; //passed into vfs_lookup function
+        char* filenamecast = kmalloc(sizeof((char *) NAME_MAX));
+        strcpy(filenamecast, filename); //cast constant filename -> non-constant
+        int ret = vfs_lookup(filenamecast, new_file); //takes care of EISDIR and ENOTDIR hopefully
 
-	struct fd* f = create_fd(file_flag, filename, *new_file); //add fd to the fd table
-	add_fd(f,file_handle);
+        if(file_flag & O_CREAT && ret >= 0 && file_flag & O_EXCL){
+                errno = EEXIST; //file creation failed because it already exists
+                return -1;
+        }
+	else if (ret < 0){
+		create = true;
+		if (!(file_flag & O_CREAT)){
+                        errno = ENOENT;
+                        return -1;
+                }
+	}
+        //new_file = NULL; //passed into vfs_open function
+        if(curproc->open_num < MAX_fd_table){  //fd table is not full, open allowed 
+                ret = vfs_open(filenamecast, file_flag, mode , new_file);
+                curproc->open_num++;
+                if(ret < 0){
+                        return -1;
+                }
+        }else{ //fd table is full, open disallowed
+		if (create) errno = ENOSPC;
+		else errno = EMFILE;
+                return -1;
+        }
+        
+        //still need to check errors: ENOSPC and EIO********************************************************************        
+        
+        int file_handle = 3; //file handle is the index at which the fd is located
+        
+        while(curproc->fd_table[file_handle] != NULL) { //find empty slot in fd_table
+                file_handle++;
+        }
 
-	return file_handle;  //index of the fd in the fd_fd_table
+        struct fd* f = create_fd(file_flag, filename, *new_file); //add fd to the fd table
+        add_fd(f,file_handle);
+
+        return file_handle;  //index of the fd in the fd_fd_table
 }
 
 
@@ -121,7 +129,6 @@ int sys_read(int fd, void *buf, size_t buflen) {
         struct uio u;
         struct iovec iov;
         struct addrspace *as ;
-        int retval;
         as = as_create();
 
         iov.iov_ubase = buf;
@@ -135,8 +142,12 @@ int sys_read(int fd, void *buf, size_t buflen) {
         u.uio_rw = UIO_READ;
         u.uio_space = curproc_getas();
 
-        retval = VOP_READ(tmp->file, &u);
-        return buflen - u.uio_resid;
+        int retval = VOP_READ(tmp->file, &u);
+	if (retval < 0){
+		errno = EIO;
+		return -1;
+	}
+        return buflen - u.uio_resid ;
 }
 
 int sys_write(int fd, const void *buf, size_t nbytes) {
@@ -144,27 +155,30 @@ int sys_write(int fd, const void *buf, size_t nbytes) {
                 errno = EBADF;
                 return -1;
         }
-
+        if (!buf){      // else if invalid buffer
+                errno = EFAULT;
+                return -1;
+        }
 	struct vnode *vn; // creating vnode (temp)
 	struct uio u;
 	struct iovec iov;
 	struct addrspace *as;
 
-        as = as_create();
+	as = as_create();
 
 	iov.iov_ubase = (void *)buf;
 	iov.iov_len = nbytes;
-	
+
 	u.uio_iov = &iov;
 	u.uio_resid = nbytes;
         u.uio_rw = UIO_WRITE;
         u.uio_segflg = UIO_USERSPACE;
         u.uio_space = curproc_getas();
 
-        if(fd == STDIN_FILENO){
-                errno = EIO;
-                return -1;
-        }
+	if(fd == STDIN_FILENO){
+		errno = EIO;
+		return -1;
+	}
 	else if(fd == STDOUT_FILENO || fd == STDERR_FILENO){
 		char *console = NULL; // console string ("con:")
 		console = kstrdup("con:"); // set to console
