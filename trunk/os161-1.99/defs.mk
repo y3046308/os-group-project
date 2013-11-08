80da6068903ebe9bd8a3d5be3442608d7ac8d6c4
y3046308@gmail.com <y3046308@gmail.com@280eca72-8607-5fb9-e67e-0985319f605b>
2013-11-08 04:23:58 +0000

    Exception handling at trap.c (not sure if it's correct)

--- trunk/os161-1.99/defs.mk
+++ trunk/os161-1.99/defs.mk
@@ -13,7 +13,7 @@
 # The things that can be set here are documented in mk/os161.config.mk.
 #
 
-OSTREE=/Users/CSBaek/os-group-project/root
+OSTREE=/u2/jy7yang/os-group-project/root
 PLATFORM=sys161
 MACHINE=mips
 COMPAT_CFLAGS=
