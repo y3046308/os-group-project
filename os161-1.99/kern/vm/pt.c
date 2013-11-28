0f5d58ce9d4b24676306ca5707746932a797b146
soojilee13 <soojilee13@gmail.com>
2013-11-28 04:36:21 +0000

    here is pt c

--- os161-1.99/kern/vm/pt.c
+++ os161-1.99/kern/vm/pt.c
@@ -1,19 +1,5 @@
 #include "pt.h"
 
-struct pte
-pte_create(void){
-	struct pte pte = kmalloc(sizeof(struct pte));
-	if (pte == NULL){
-		return NULL;
-	}
-	
-	pte->pfn = 0;
-	pte->valid = 0;
-	pte->dirty = 0;
-	
-	return pte;
-}
-
 struct pte 
 pte_create(paddr_t pfn, int valid, int dirty){
 	struct pte pte = kmalloc(sizeof(struct pte));
