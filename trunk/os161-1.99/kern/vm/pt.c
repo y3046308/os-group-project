0123a0497acc55f15f5455a5684b6723a98fb018
soojilee13@gmail.com <soojilee13@gmail.com@280eca72-8607-5fb9-e67e-0985319f605b>
2013-11-29 02:40:24 +0000

    check

--- trunk/os161-1.99/kern/vm/pt.c
+++ trunk/os161-1.99/kern/vm/pt.c
@@ -7,9 +7,9 @@
 struct pte 
 pte_create(paddr_t pfn, int valid, int dirty){
 	struct pte *p = kmalloc(sizeof(struct pte));
-	if (p == NULL){
+/*	if (p == NULL){
 		return NULL;
-	}
+	} */
 	
 	p->pfn = pfn;
 	p->valid = valid;
