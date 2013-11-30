ca9edc482064bf31ffba6bfeab5b7795999ee818
y3046308 <y3046308@gmail.com>
2013-11-30 03:54:45 +0000

    swapfile

--- os161-1.99/kern/include/swapfile.h
+++ os161-1.99/kern/include/swapfile.h
@@ -1,4 +1,23 @@
 #include "opt-A3.h"
+#include <types.h>
+#include <vnode.h>
+#include <synch.h>
+#include <pt.h>
 #if OPT_A3
 
+#define MAX_SIZE 9 * 1024 * 1024	// maximum swapfile size
+/*
+struct swap{
+	bool occupied;
+//	struct pte* entry;
+};*/
+typedef bool swap;
+void swap_initialize(void);
+void read_from_swap(int index);
+int write_to_swap(void);
+
+int swap_num;	// total number of pages in swapfile
+struct vnode* swapfile;
+struct lock* swapLock;
+swap* swap_partition;
 #endif
